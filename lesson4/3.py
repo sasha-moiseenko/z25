7362b4a0838e4aa65e96aa508097aeb60a6d0023
Sasha Moiseenko <nebudimore@gmail.com>
2020-02-02 19:38:46 +0300

    the third home task

--- lesson4/3.py
+++ lesson4/3.py
@@ -1,4 +1,28 @@
 # Заполнить список вещественных чисел вводом с клавиатуры.
 # Найте элементы списка, которые меньше среднего арифметического.
 
-# add code here
+_list = []
+_sum = 0
+_avg = 0
+small_list = []
+try:
+    while True:
+        number = input('> ')
+        if not number:
+            break
+        number = int(number)
+        _list.append(number)
+except ValueError:
+    print('Введите корректное число')
+try:
+    for i in _list:
+        _sum += i
+    _avg = _sum / len(_list)
+except ZeroDivisionError:
+    print('Деление на ноль запрещено: проверьте вводные данные')
+    for i in _list:
+        if i < _avg:
+            small_list.append(i)
+print('среднее арифметическое > ', _avg, small_list)
+
+
