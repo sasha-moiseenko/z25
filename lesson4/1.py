1bd94cfdd393f8fb4f9bd1d19dca96754dbc5119
northsea-coder <nebudimore@gmail.com>
2020-02-02 13:24:53 +0300

    the first home task

--- lesson4/1.py
+++ lesson4/1.py
@@ -3,4 +3,12 @@
 # Вывести на экран сообщение об этом, а также остаток (если он есть)
 # и частное (в любом случае).
 
-# add code here
+try:
+    a = int(input('enter the number > '))
+    b = int(input('enter the number > '))
+    if a % b == 0:
+        print('a делится на b, ', 'a / b =', a / b)
+    else:
+        print('a не делится на b,', 'a / b =', a / b, ',остаток = ', a % b)
+except ZeroDivisionError as exc:
+    print(exc, '> введите корректное число в делитель')
