646bb8c6085a5f10bb788969e13ebc74e9a6101c
Sasha Moiseenko <nebudimore@gmail.com>
2020-02-02 20:38:33 +0300

    2nd home task

--- lesson4/2.py
+++ lesson4/2.py
@@ -1,4 +1,21 @@
 # Заполнить список вещественных чисел вводом с клавиатуры.
 # Сколько элементов списка больше по модулю максимального числа.
 
-# add code here
+_list = []
+_max = 0
+_count = 0
+try:
+    while True:
+        number = input('> ')
+        if not number:
+            break
+        number = int(number)
+        _list.append(number)
+except ValueError:
+    print('Введите корректное число')
+for i in _list:
+    if i > _max:
+        _max = i
+    if abs(i) > _max:
+        _count += 1
+print(_count, _max)
