75f181ee4999adb10654bc8ae6b46fbf87ff89f3
tima <tima.akulich@gmail.com>
2020-02-04 19:24:45 +0300

    Task 1

--- lesson4/1.py
+++ lesson4/1.py
@@ -3,4 +3,16 @@
 # Вывести на экран сообщение об этом, а также остаток (если он есть)
 # и частное (в любом случае).
 
-# add code here
+try:
+    num_1 = int(input("Number 1 ? >"))
+    num_2 = int(input("Number 2 ? >"))
+except ValueError:
+    print('Error, not a number')
+else:
+    if not num_2:
+        print('Zero division')
+    else:
+        mod = num_1 % num_2
+        div = num_1 // num_2
+        print('Делится' if mod else 'Не делится')
+        print(div, mod)
