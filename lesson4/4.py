e377d7ac9b095a363230f14fd453a8964fb06f51
tima <tima.akulich@gmail.com>
2020-02-04 19:37:02 +0300

    task4

--- lesson4/4.py
+++ lesson4/4.py
@@ -2,3 +2,16 @@
 # в байты или килобайты в зависимости от его выбора.
 
 # add code here
+try:
+    number = int(input('Number ? >'))
+except ValueError:
+    print('Invalid number')
+else:
+    choice = input('Bytes(b) or kBytes(k)? > ')
+    value = None
+    if choice == 'b':
+        value = number * 1024
+    elif choice == 'k':
+        value = number / 1024
+    else:
+        print('Invalid choice')
