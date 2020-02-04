2502293a7a30b30bd7b7746421c8b0e9c4544532
tima <tima.akulich@gmail.com>
2020-02-04 19:30:31 +0300

    task 2

--- lesson4/2.py
+++ lesson4/2.py
@@ -2,3 +2,22 @@
 # Сколько элементов списка больше по модулю максимального числа.
 
 # add code here
+_list = []
+
+while True:
+    user_number = input('Number ? > ')
+    if not user_number:
+        break
+    try:
+        _list.append(float(user_number))
+    except ValueError:
+        print('Wrong number')
+
+max_number = max(_list)
+
+count = 0
+for number in _list:
+    if abs(number) > max_number:
+        count += 1
+
+print(count)
